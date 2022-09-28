384815ef0fd3acdb13c2dbc36f4f8131245cb4f3
liyuhao <yuhao.li@wowjoy.cn>
2022-09-28 18:16:52 +0800

    图标修改

--- packages/icon-svg/plugins/svg2Definition/transforms/presets/adjustViewBox.ts
+++ packages/icon-svg/plugins/svg2Definition/transforms/presets/adjustViewBox.ts
@@ -8,6 +8,7 @@ const SPECIAL_ICON_NAMES = {
   'top':  '0 0 1024 1024',
   'top-hover':  '0 0 1024 1024',
   'right2': '0 0 12 10',
+  'search-no-data': '0 0 61 60'
 };
 
 export const adjustViewBox: TransformFactory = assignAttrsAtTag(
