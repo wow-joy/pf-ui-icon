c9995ccf1418f2078939490264dc687a406c53b9
xuzhudong <zhudong.xu@wowjoy.cn>
2022-10-12 17:12:04 +0800

    feat: 搭建icons-react组件库

---
+++ packages/icons-react/__tests__/icons-react.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const iconsReact = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(iconsReact(), 'Hello from iconsReact');
+console.info("iconsReact tests passed");
